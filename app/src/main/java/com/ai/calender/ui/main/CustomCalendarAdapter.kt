c6c1f5859816d74288522fb2625784abcd3bb4e6
satoru2111 <satoru2111@gmail.com>
2019-12-12 01:00:41 +0900

    feat: ライブラリ使用

--- app/src/main/java/com/ai/calender/ui/main/CustomCalendarAdapter.kt
+++ app/src/main/java/com/ai/calender/ui/main/CustomCalendarAdapter.kt
@@ -1,5 +1,6 @@
 package com.ai.calender.ui.main
 
+import ajd4jp.Month
 import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
@@ -7,19 +8,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
-import com.ai.calender.R
 import jp.kuluna.calendarviewpager.CalendarPagerAdapter
 import jp.kuluna.calendarviewpager.Day
 import jp.kuluna.calendarviewpager.DayState
 import java.util.*
+import ajd4jp.Holiday
+import ajd4jp.LunisolarYear
+import ajd4jp.AJD
+import android.os.SystemClock
+import android.util.Log
+import com.ai.calender.R
+
 
 class CustomCalendarAdapter(context: Context) : CalendarPagerAdapter(context) {
+    val ly = LunisolarYear.getLunisolarYear(AJD(Calendar.getInstance()))
     override fun onCreateView(parent: ViewGroup, viewType: Int): View {
         return LayoutInflater.from(context).inflate(R.layout.view_calendar_cell, parent, false)
     }
 
     override fun onBindView(view: View, day: Day) {
         if (day.state == DayState.ThisMonth) {
+
             // 日付
             view.findViewById<TextView>(R.id.text_day).text =
                 day.calendar.get(Calendar.DAY_OF_MONTH).toString()
@@ -29,12 +38,43 @@ class CustomCalendarAdapter(context: Context) : CalendarPagerAdapter(context) {
                 Calendar.SATURDAY -> view.findViewById<TextView>(R.id.text_day).setTextColor(Color.BLUE)
                 else -> view.findViewById<TextView>(R.id.text_day).setTextColor(Color.BLACK)
             }
+
+            // 休日
+            if (isHoliday(day)) view.findViewById<TextView>(R.id.text_day).setTextColor(Color.RED)
+
             // 背景
             if (day.isSelected) view.findViewById<ConstraintLayout>(R.id.background).setBackgroundColor(
                 Color.GRAY
             )
+
+            // 六曜
+            view.findViewById<TextView>(R.id.text_rokuyou).text = getRokuyou(day.calendar)
         } else {
             view.findViewById<TextView>(R.id.text_day).text = ""
         }
+    }
+
+    private fun isHoliday(day: Day): Boolean {
+        val days = Month(day.calendar.get(Calendar.YEAR), day.calendar.get(Calendar.MONTH) + 1).days
+        var holiday: Holiday? = null
+        days.find { it.day == day.calendar.get(Calendar.DAY_OF_MONTH) }?.also {
+            holiday = Holiday.getHoliday(it)
+        }
+        return holiday != null
+    }
+
+    @Synchronized
+    private fun getRokuyou(calendar: Calendar): String {
+        val start = SystemClock.currentThreadTimeMillis()
+        val ajd = AJD(calendar)
+        val ajd2 = AJD(calendar)
+        val end = SystemClock.currentThreadTimeMillis()
+        Log.e("test", "create AJD time = " + (end - start))
+        return LunisolarYear.getLunisolarYear(ajd).getLSCD(ajd2).rokuyo.getName()
+//        return ""
+
+//        val lscd = ly.getLSCD(ajd)
+//        val rokuyou = lscd.rokuyo.getName()
+//        return rokuyou
     }
 }
